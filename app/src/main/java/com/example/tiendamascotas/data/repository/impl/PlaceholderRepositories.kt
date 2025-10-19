6541d5138882ebca7cb503f2a9e1b878c0f3992d
GussRamirez0 <lramirezb10@miumg.edu.gt>
2025-10-19 09:43:27 -0600

    App con 2 botones profile y reportes

--- app/src/main/java/com/example/tiendamascotas/data/repository/impl/PlaceholderRepositories.kt
+++ app/src/main/java/com/example/tiendamascotas/data/repository/impl/PlaceholderRepositories.kt
@@ -2,7 +2,6 @@ package com.example.tiendamascotas.data.repository.impl
 
 import com.example.tiendamascotas.domain.repository.*
 
-class ReportsRepositoryImpl : ReportsRepository { /* TODO */ }
 class ReviewsRepositoryImpl : ReviewsRepository { /* TODO */ }
 class ChatRepositoryImpl : ChatRepository { /* TODO */ }
 class AdoptionsRepositoryImpl : AdoptionsRepository { /* TODO */ }
